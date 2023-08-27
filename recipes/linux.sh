5dc921a981033887bf7ed79d37adddff6168a82c
henrik <>
2023-08-27 11:55:12 +0200

    Update Linux to 6.1.48

--- recipes/linux.sh
+++ recipes/linux.sh
@@ -3,8 +3,8 @@ set -e
 script_dir="$(cd -- "$(dirname -- "$0")" && pwd)"
 . "$script_dir/../hc/bootstrap/recipe.sh"
 
-URL="https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.1.27.tar.xz"
-SHA256="c2b74b96dd3d0cc9f300914ef7c4eef76d5fac9de6047961f49e69447ce9f905"
+URL="https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.1.48.tar.xz"
+SHA256="c606cbd0353e677df6fae73cc16ba3c9244b98372ed7771d551024016f55ac31"
 DEPENDENCIES="../hc/bootstrap/make.sh ../hc/bootstrap/xz.sh ./llvm.sh ./bc.sh ./flex.sh ./bison.sh ./elfutils.sh ./linux-firmware.sh"
 FILE_DEPENDENCIES="files/linux/initramfs files/linux/.config files/linux/regulatory.db ../src/init/x86_64/init.elf ../src/router/x86_64/router.elf"
 
