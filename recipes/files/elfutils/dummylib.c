0b3677066064f85ecf1c4fdab8ba2113c283d259
henrik <>
2024-05-20 21:40:40 +0200

    elfutils recipe cleanup

--- recipes/files/elfutils/dummylib.c
+++
@@ -1,5 +0,0 @@
-// elfutils is a pain to build on musl..
-// Trick the configure script to avoid unneeded dependencies.
-void argp_parse(void) {}
-void fts_close(void) {}
-void _obstack_free(void) {}
--- recipes/files/elfutils/libintl.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LIBINTL_H
-#define _LIBINTL_H
-
-#define dgettext(DOMAIN, STR) (STR)
-
-#endif
