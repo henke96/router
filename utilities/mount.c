1b8fc4e7e7a4c9e45f600e4e12504dfce42ec34a
henrik <>
2024-06-02 11:10:06 +0200

    Add umount program

--- utilities/mount.c
+++ utilities/mount.c
@@ -5,9 +5,11 @@
 #include "hc/linux/helpers/_start.c"
 
 int32_t start(int32_t argc, char **argv, hc_UNUSED char **envp) {
-    if (argc < 4 || sys_mount(argv[1], argv[2], argv[3], MS_NOATIME, NULL) < 0) {
-        sys_write(1, hc_STR_COMMA_LEN("Usage: mount DEVICE MOUNTPOINT FSTYPE\n"));
-        return 1;
-    }
-    return 0;
+    if (argc == 4 && sys_mount(argv[1], argv[2], argv[3], MS_NOATIME, NULL) == 0) return 0;
+    if (argc == 2 && sys_umount2(argv[1], 0) == 0) return 0;
+    sys_write(1, hc_STR_COMMA_LEN(
+        "Mounting: mount /dev/sda1 /mnt vfat\n"
+        "Unmounting: mount /mnt\n"
+    ));
+    return 1;
 }
