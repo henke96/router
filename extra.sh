9c05a9d9c66babda4df205e2d2a1acdc52591f74
henrik <>
2024-09-15 10:29:17 +0200

    Add util-linux recipe to extras

--- extra.sh
+++ extra.sh
@@ -3,14 +3,13 @@ set -e
 script_dir="$(cd -- "${0%/*}/" && pwd)"
 OUT="$(cd -- "$OUT" && pwd)"
 
-for pkg in bash e2fsprogs nano ncurses; do
+for pkg in bash e2fsprogs nano ncurses util-linux; do
     "$script_dir/recipes/extra/$pkg"
     ! test -d "$OUT/$pkg/bin" || export PATH="$OUT/$pkg/bin:$PATH"
     ! test -d "$OUT/$pkg/sbin" || export PATH="$OUT/$pkg/sbin:$PATH"
 done
 
 for pkg in bc bison bzip2 cmake elfutils flex m4 make mtools perl python xorriso xz; do
-    "$script_dir/recipes/$pkg"
     ! test -d "$OUT/$pkg/bin" || export PATH="$OUT/$pkg/bin:$PATH"
     ! test -d "$OUT/$pkg/sbin" || export PATH="$OUT/$pkg/sbin:$PATH"
 done
